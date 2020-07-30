561551ffa01d99b7226dc3722ee7e22129f2309e
SergeyBalakirev <balakirev.sergey87@gmail.com>
2020-07-30 21:48:36 +0300

    added fullpage

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,15 @@
 $(function () {
 
+  new fullpage('#fullpage', {
 
+    autoScrolling: true,
+    navigation: true,
+    navigationPosition: 'left',
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+    navigationTooltips: ['ГЛАВНАЯ', 'ГАЛЕРЕЯ', 'ПЛАНИРОВКИ', 'ДОКУМЕНТЫ', 'КОНТАКТЫ'],
+    showActiveTooltip: true,
+  });
 
 
 });
